f251d16c8ed3ceac30586cb0ad1fe0a5e2493d99
major-phyo-san <phyojupiter9@gamil.com>
2020-10-11 20:43:44 +0630

    Template engine changed to Handlebars. Set default layout and error pages.

--- app.js
+++ app.js
@@ -1,48 +1,86 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
+var handlebars = require('express-handlebars');
 var cookieParser = require('cookie-parser');
+
+var createError = require('http-errors');
 var logger = require('morgan');
 
+var path = require('path');
+
+// import configs
 var envs = require('./config/config');
+
+// initialize Express and Handlebars
+var app = express();
+var hbs = handlebars;
+
+//=======================================================//
+// view engine setup with Handlebars
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '/app/views'));
+app.engine('hbs', hbs({  
+  layoutsDir:path.join(__dirname, '/app/views/layouts'), 
+  defaultLayout:'main',
+  extname:'.hbs'
+}));
+//=======================================================//
+
+// serve static assets
+app.use(express.static(path.join(__dirname, 'public')));
 
 // import routes
 var indexRouter = require('./routes/web/index');
 var usersRouter = require('./routes/web/users');
 
-var app = express();
-
-//======================== use routes=====================//
+//=======================================================//
+// use imported routes
 app.use(indexRouter);
 app.use(usersRouter);
-//=======================================================//
-
-//=======================================================//
-// view engine setup
-app.set('views', path.join(__dirname, 'app/views'));
-app.set('view engine', 'ejs');
 //=======================================================//
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-// catch 404 and forward to error handler
+// catch 404 and render the error page
 app.use(function(req, res, next) {
-  next(createError(404));
+  res.status(404);
+  var msg = '404 Not found';
+  if (req.accepts() == 'application/json') {
+    res.send({
+      'isSuccess': false,
+      'message': 'error occured',
+      'error type': msg
+    });
+  }
+
+  else {
+    res.render('errorpages/error', { errorMsg: msg });
+  }
 });
 
-// error handler
+// catch 500 error and render the error page
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = envs.NODE_ENV === 'development' ? err : {};
+  
+  var statusCode = err.status || 500;
+  res.status(statusCode);
+  var msg = statusCode + ' Server error';
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  if (req.accepts() == 'application/json') {
+    res.send({
+      'isSuccess': false,
+      'message': 'error occured',
+      'error type': msg
+    });
+  }
+
+  else {
+    res.render('errorpages/error', { errorMsg: msg });
+  }
 });
 
 module.exports = app;
