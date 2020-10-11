3c9c988b7b974d00922846fa337755555479889b
major-phyo-san <phyojupiter9@gamil.com>
2020-10-11 20:46:10 +0630

    render default index page

--- routes/web/index.js
+++ routes/web/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Julia' });
+  res.render('index', {appName: 'Julia'});
 });
 
 module.exports = router;
