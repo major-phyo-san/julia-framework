15096f6a082a5fc16984192a223cec8cd188b982
major-phyo-san <phyojupiter9@gamil.com>
2020-10-17 21:50:37 +0630

    sidebar partial added

--- routes/web/index.js
+++ routes/web/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {appName: 'Julia'});
+  res.render('index', {appName: 'Julia', activePage: 'Index is active'});
 });
 
 module.exports = router;
