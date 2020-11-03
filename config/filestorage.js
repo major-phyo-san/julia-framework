e3cef35b1ab27cbe757c61294ae00f8e388ab311
major-phyo-san <phyojupiter9@gamil.com>
2020-11-03 12:04:40 +0630

    file storage module added

--- config/filestorage.js
+++ config/filestorage.js
@@ -1,20 +1,21 @@
 var multer = require('multer');
 var path = require('path');
+var stringGenerator = require('./randomstrings');
 
-var baseDir = '../storage/app';
+const baseDir = './storage/app';
 
 var makeFileStorage = function(dir){
     var fullDir = path.join(baseDir, dir);
     var relativeDir = path.relative(process.cwd(), fullDir);
-    //console.log(fullDir);
-    //console.log(relativeDir);
+    
     var storage = multer.diskStorage({
         destination: function(req, file, cb){
             cb(null, relativeDir);
         },
-        filename: function(req, file, cb){            
-            cb(null, file.originalname);
-        }
+        filename:function(req, file,cb){
+            var randomFileName = stringGenerator(5) + '_' + stringGenerator(5) + '_' + stringGenerator(5);
+            cb(null, randomFileName + path.extname(file.originalname));
+          }
     });
     
     return storage;
