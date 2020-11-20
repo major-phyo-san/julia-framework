9a0ac0f440c1e89be7f6494ec6f7c81495fc615f
major-phyo-san <phyojupiter9@gamil.com>
2020-11-20 13:12:06 +0630

    App refactored for Express-WS websocket support. Dependencies loading re-structured. CSRF protection error fixed.

--- server.js
+++ server.js
@@ -1,74 +1,94 @@
-// express and express handlebars
+//=======================================================//
+// dependencies loading section
+var http = require('http');
+var path = require('path');
+
+// load main dependencies for Express
 var express = require('express');
+var expressWs = require('express-ws');
 var handlebars = require('express-handlebars');
 
+// load cookie parser and CSRF token guard
 var cookieParser = require('cookie-parser');
+var csrfGuard = require('csurf');
 
 // for HTTP requests logging
-var devLogger = require('morgan'); // development logger
-var prodLogger = require('express-logger'); // production logger
-
+var developmentLogger = require('morgan');
+var productionLogger = require('express-logger');
 var createError = require('http-errors');
-
-var path = require('path');
 
 // import configs
 var envs = require('./config/server-env');
 
-// initialize Express and Handlebars
-var server = express();
-server.set('env', envs.NODE_ENV);
+//=======================================================//
+
+// initialize Express app and create server for the app
+var app = express();
+app.server = http.createServer(app);
+
+// set app environment
+app.set('env', envs.NODE_ENV);
+
+// initialize Express-WS
+expressWs(app, app.server);
+
+// initialize Handlebars
 var hbs = handlebars;
 
 //=======================================================//
 // view engine setup section
 
 // Handlebars setup
-server.set('view engine', 'hbs');
-server.set('views', path.join(__dirname, '/app/views'));
-server.engine('hbs', hbs({  
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '/app/views'));
+app.engine('hbs', hbs({  
   layoutsDir:path.join(__dirname, '/app/views/layouts'), 
   defaultLayout:'main',
   extname:'.hbs'
 }));
 
 // cache templates in production
-if(server.get('env') === 'production'){  
-  server.enable('view cache');
+if(app.get('env') === 'production'){  
+  app.enable('view cache');
 }
+
 //=======================================================//
 
 // deal with uncaught error
 var handlerUncaught = require('./app/controllers/middlewares/ErrorHandlers/uncaughtErrorHandler');
-server.use(handlerUncaught);
+app.use(handlerUncaught);
 
 // serve static assets
-server.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //=======================================================//
-// server features setup section
+// app features setup section
 
-if(server.get('env') === 'development'){
-  server.use(devLogger('dev'));
+if(app.get('env') === 'development'){
+  app.use(developmentLogger('dev'));
 }
-if(server.get('env') === 'test'){
-  server.use(devLogger('combined'));
+if(app.get('env') === 'test'){
+  app.use(developmentLogger('combined'));
 }
-if(server.get('env') === 'production'){
-  server.use(prodLogger({
+if(app.get('env') === 'production'){
+  app.use(productionLogger({
     path: __dirname + '/storage/logs/requests.log'
   }));
 }
 
-server.use(express.json());
-server.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // encrypt cookies with cookie secret key from env
-server.use(cookieParser(envs.NODE_COOKIES_SECRET));
-server.use(require('csurf')());
-server.use(function(req, res, next){
+app.use(cookieParser(envs.NODE_COOKIES_SECRET));
+
+// add CSRF token guard to app
+app.use(csrfGuard({cookie: true}));
+app.use(function(req, res, next){
   res.locals._csrfToken = req.csrfToken();
   next();
 });
+
 //=======================================================//
 
 //=======================================================//
@@ -79,8 +99,9 @@ var indexRouter = require('./routes/web/index');
 var usersRouter = require('./routes/web/users');
 
 // use imported routes
-server.use(indexRouter);
-server.use(usersRouter);
+app.use(indexRouter);
+app.use(usersRouter);
+
 //=======================================================//
 
 //=======================================================//
@@ -89,10 +110,10 @@ var handler404 = require('./app/controllers/middlewares/ErrorHandlers/notFoundEr
 var handler500 = require('./app/controllers/middlewares/ErrorHandlers/serverErrorHandler');
 
 // catch 404 and render the error page
-server.use(handler404);
+app.use(handler404);
 
 // catch 500 error and render the error page
-server.use(handler500);
+app.use(handler500);
 //=======================================================//
 
-module.exports = server;
+module.exports = app;
