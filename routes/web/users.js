c9126d166635930da03aa966e32afbdcf1ceb10f
major-phyo-san <phyojupiter9@gamil.com>
2020-11-22 21:07:22 +0630

    multi files routing removed

--- routes/web/users.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var UserController = require('../../app/controllers/UserController');
-
-/* GET users listing. */
-router.get('/users', UserController.index);
-
-module.exports = router;
