5192e97a369cc091c5c08235e2de145ab8a0c278
major-phyo-san <phyojupiter9@gamil.com>
2020-12-01 09:54:54 +0630

    Database config modified

--- config/database.js
+++ config/database.js
@@ -1,24 +1,23 @@
-// this module provides storage option for multer file uploads
+// this module provides database connections for persistent storage
 // created on 08-11-2020
+// modified on 01-12-2020
 
 var mongoose = require('mongoose');
 var envs = require('./server-env');
 
-var options = {    
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-};
-
 var connection = null;
 
-const primaryConnectionString = envs.DB_CONNECTION + envs.DB_USERNAME + ':' + envs.DB_PASSWORD + '@' + envs.DB_HOST + ':' + envs.DB_PORT + '/' + envs.DB_DATABASE;
-const optionalConnectionString = '?authSource=admin&compressors=snappy&gssapiServiceName=mongodb';
-const connectionString = primaryConnectionString + optionalConnectionString;
+module.exports.makeMongoDBConnection = function(optionalConnectionString, serverOptions){
+    // MongoDB server credentials from environment variables
+    const primaryConnectionString = envs.DB_CONNECTION + envs.DB_USERNAME + ':' + envs.DB_PASSWORD + '@' + envs.DB_HOST + ':' + envs.DB_PORT + '/' + envs.DB_DATABASE + '?';
+    // complete connection string will be 'mongodb://{username:password}@hostname:portnumber/dbname?optionalconnectionstring
+    const connectionString = primaryConnectionString + new URLSearchParams(optionalConnectionString).toString();    
 
-mongoose.connect(connectionString, options).then(function(result){
-    connection = result;
-}, function(error){
-    connection = null;
-});
+    mongoose.connect(connectionString, serverOptions).then(function(result){
+        connection = mongoose.connection;
+    }, function(error){
+        connection = null;
+    });
 
-module.exports = connection;
+    return connection;
+}
