6bab9e40bbdb3d59cebee8d918ab12449cf7d7a1
major-phyo-san <phyojupiter9@gamil.com>
2020-12-01 13:56:04 +0630

    Added middleware for method-override

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ var express = require('express');
 var expressWs = require('express-ws');
 var handlebars = require('express-handlebars');
 
-// load cookie parser and CSRF token guard
+// load cookie parser, CSRF token guard and method override
 var cookieParser = require('cookie-parser');
 var csrfGuard = require('csurf');
+var methodOverride = require('method-override');
 
 // for HTTP requests logging
 var developmentLogger = require('morgan');
@@ -80,11 +81,24 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(envs.NODE_COOKIES_SECRET));
 
 // add CSRF token guard to app
+// check for value in '_csrf' attribute of hidden inputs
 app.use(csrfGuard({cookie: true}));
 app.use(function(req, res, next){
   res.locals._csrfToken = req.csrfToken();
   next();
 });
+
+// add method override
+// check for value in '_method' attribute of hidden inputs
+app.use(methodOverride(function(req, res, next){
+  if(req.body && typeof req.body === 'object' && '_method' in req.body){
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
+// check 'X-HTTP-Method-Override' in Ajax request headers
+app.use(methodOverride('X-HTTP-Method-Override'));
 
 //=======================================================//
 
