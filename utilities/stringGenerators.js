8c4d3a80fd793fb6fbc021768cefe8cf5a68dc9b
major-phyo-san <phyojupiter9@gamil.com>
2020-12-09 18:50:05 +0630

    Feature: Added generateUUID() for uuid v4 generation

--- utilities/stringGenerators.js
+++ utilities/stringGenerators.js
@@ -1,6 +1,8 @@
 // this module generates a random string based on the 'length' value
 // created on 03-11-2020
 
+var uuid = require('uuid');
+
 module.exports.generateRandomString = function (length) {
    // result will be the generated string
    var result = '';
@@ -14,4 +16,9 @@ module.exports.generateRandomString = function (length) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
    }
    return result;
+}
+
+// module to generate uuid v4
+module.exports.generateUUID = function(){
+   return uuid.v4();
 }
