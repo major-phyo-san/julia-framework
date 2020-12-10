f0d7cbadf60734c8f1ea998cb3b050ef2233f87e
major-phyo-san <phyojupiter9@gamil.com>
2020-12-10 14:32:21 +0630

    Feature: base64digest() added

--- utilities/cryptography.js
+++ utilities/cryptography.js
@@ -2,3 +2,12 @@
 // created on 10-12-2020
 
 var crypto = require('crypto');
+
+// base64digest() function takes a String parameter
+// and return based64 digested form
+module.exports.base64digest = function(str){
+    var hasher = crypto.createHash('sha256');
+    hasher.update(str, 'utf8');
+    digestedStr = hasher.digest('base64');
+    return digestedStr;
+}
