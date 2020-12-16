02893135dfb4b8d89f09be150ab604d38cf22ebd
major-phyo-san <phyojupiter9@gamil.com>
2020-12-16 11:27:23 +0630

    Feature: added auth module to provide user authentication with Passport.

---
+++ config/auth.js
@@ -0,0 +1,100 @@
+// this module provides user authentication using Passport
+// created on 13-12-2020
+
+const database = require('./database');
+const cryptography = require('../utilities/cryptography');
+const User = require('../app/models/User');
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+var optionalConnectionString = {
+    authSource: 'admin',
+    compressors: 'zlib',
+    gssapiServiceName: 'mongodb'
+};
+
+var serverOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+};
+
+const connection = database.makeMongoDBConnection(optionalConnectionString, serverOptions);
+
+module.exports.makePassportAuth = function(){
+    passport.use('local', new LocalStrategy(
+        { usernameField: 'email' },
+        function (email, password, done) {
+            // inside local strategy callback
+            var errorObj = {"message": "failed to authenticate user"};
+            console.log('authenticating user using local strtegy');
+    
+            // query the database for user with provided credentials
+            User.findOne({ email: email }, function (err, user) {
+                // database error
+                // local strategy returned false
+                if (err) {
+                    console.log('error occurred in authentication database');
+                    errorObj["reason"] = "auth db err";
+                    return done(errorObj, null);
+                }
+    
+                // user found, provided password needs to be checked
+                // against the hashed (bcrypt) password stored in the db
+                if (user) {
+                    console.log('user found in db');
+                    // checking the password
+                    cryptography.debcrypt(password, user.password, function (match) {
+                        // password check 'OK' 
+                        // local strategy returned true
+                        if (match) {
+                            console.log('valid password');
+                            return done(null, user);
+                        }
+                        // password check 'NOT OK'
+                        // local strategy returned false
+                        else {
+                            console.log('invalid password');
+                            errorObj["reason"] = "invalid password";
+                            return done(errorObj, null);
+                        }
+                    });
+                }
+    
+                // user not found
+                // local strategy returned false
+                else {
+                    console.log('user not found in db');
+                    errorObj["reason"] = "invalid credential";
+                    return done(errorObj, null);
+                }
+    
+            });
+        }
+    ));
+    
+    passport.serializeUser(function (user, done) {
+        // serialize user callback
+        // saves the user id to the session file store
+        // saves the user id in req.session.passport (req.session.passport.user_id)
+        // adds the user obj in req obj (req.user)
+        done(null, user.id);
+    });
+    
+    passport.deserializeUser(function (id, done) {        
+        // de-serialiize user callback
+        // query the database for user with the id stored in the passport session (req.session.passport.user_id)
+        User.findById(id, function(err, user){
+            if(err){
+                // user is not restored from db
+                done(err, null);
+            }
+            if(user){
+                // user is restored from db
+                done(null, user);
+            }
+        });
+    });
+
+    return passport;
+}
