d17cd03076e36c190ca1e57e3d9da01d861b59bd
major-phyo-san <phyojupiter9@gmail.com>
2020-12-24 09:42:59 +0630

    Feature: added default auth routes in api routes

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var auth = require('../config/auth');
+
+var authValidator = require('../app/controllers/middlewares/authValidator');
+
 /* GET api index page. */
 router.get('/api', function(req, res, next) {
     res.set({
         'Content-Type': 'application/json',
         'Content-Length': '122'
     }).send({message: 'welcome'});    
+});
+
+router.post('/api/login', auth.makeJWTAuth);
+
+router.get('/api/protected', authValidator.validateAPIUser, function(req, res){
+    res.send({"message": "authenticated user"});
 });
 
 module.exports = router;
