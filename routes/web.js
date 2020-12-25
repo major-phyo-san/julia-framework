4d6da32a10a88c52683f795a1851b69ecf7aacbe
major-phyo-san <phyojupiter9@gmail.com>
2020-12-25 22:21:28 +0630

    Framework: added protected Web route to check authValidator

--- routes/web.js
+++ routes/web.js
@@ -1,17 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+var authValidator = require('../app/controllers/middlewares/authValidator');
+
 var LoginController = require('../app/controllers/auth/LoginController');
 var RegisterController = require('../app/controllers/auth/RegisterController');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
     res.render('index', {appName: 'Julia', activePage: 'Index is active'});
 });
 
-/* authentication and registration */
+/* protected route to test auth validator */
+router.get('/protected', authValidator.validateWebUser, function(req, res){
+    res.send('authenticated user');
+});
+
+/* Web authentication routes */
 router.get('/login', LoginController.showLoginForm);
 router.post('/login', LoginController.login);
+
+/* registration route */
 router.get('/register', RegisterController.showRegisterForm);
 router.post('/register', RegisterController.registerUser);
 
